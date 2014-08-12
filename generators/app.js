86c8d1317e244bfbf0032ffbdf24f67223d2bf79
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-08-12 12:13:28 -0400

    Generate package manifest.

--- generators/app.js
+++ generators/app.js
@@ -1,6 +1,8 @@
 "use strict";
 var Q      = require("q");
+var util   = require("util");
 var yeoman = require("yeoman-generator");
+var _      = require("lodash");
 
 module.exports = yeoman.generators.Base.extend({
 
@@ -10,14 +12,63 @@ module.exports = yeoman.generators.Base.extend({
 		"gulp", "gulp-jscs", "gulp-jshint", "gulp-lab", "jshint-stylish", "lab"
 	],
 
-	configure : function () {
+	manifest : {
+		scripts : {
+			coverage : "gulp coverage",
+			test     : "gulp"
+		}
+	},
+
+	prompting : function () {
+		var done = this.async();
+
+		this.prompt(
+			[
+				{
+					type    : "input",
+					name    : "name",
+					message : "name",
+					default : this.appname
+				},
+				{
+					type    : "input",
+					name    : "version",
+					message : "version",
+					default : "0.0.0"
+				},
+				{
+					type    : "input",
+					name    : "description",
+					message : "description"
+				},
+				{
+					type    : "input",
+					name    : "author",
+					message : "author",
+					default : util.format("%s <%s>", this.user.git.name(), this.user.git.email())
+				},
+				{
+					type    : "input",
+					name    : "license",
+					message : "license",
+					default : "MIT"
+				}
+			],
+			function (answers) {
+				_.merge(this.manifest, answers);
+				done();
+			}.bind(this)
+		);
+	},
+
+	configuring : function () {
 		this.src.copy("gulpfile.js", "gulpfile.js");
 		this.src.copy("jshint_source.json", ".jshintrc");
 		this.src.copy("jshint_test.json", "test/.jshintrc");
 		this.src.copy("jscs.json", ".jscsrc");
 		this.src.copy("gitignore", ".gitignore");
 
-		this.dest.write("package.json", JSON.stringify({}));
+		this.dest.write("package.json", JSON.stringify(this.manifest));
 	},
 
 	install : function () {
--- test/app_spec.js
+++ test/app_spec.js
@@ -5,6 +5,7 @@ var helpers = require("yeoman-generator").test;
 var Lab     = require("lab");
 var path    = require("path");
 var script  = exports.lab = Lab.script();
+var shell   = require("shelljs");
 var _       = require("lodash");
 
 var before   = script.before;
@@ -13,6 +14,8 @@ var expect   = Lab.expect;
 var it       = script.it;
 
 describe("node:app", function () {
+	var MANIFEST = "package.json";
+
 	function checkDependencies (dependencies, loaded) {
 		_.each(dependencies, function (dependency) {
 			assert.file("node_modules/" + dependency);
@@ -21,14 +24,22 @@ describe("node:app", function () {
 	}
 
 	function packageDependencies (dev) {
-		var manifest = file.readJSON("package.json");
+		var manifest = file.readJSON(MANIFEST);
 		return _.keys(manifest[ dev ? "devDependencies" : "dependencies" ]);
 	}
 
 	before(function (done) {
+		var tmp = path.join(__dirname, "tmp");
+
 		helpers.run(path.join(__dirname, "..", "generators", "app"))
-		.inDir(path.join(__dirname, "tmp"))
-		.on("end", done);
+		.inDir(tmp)
+		.once("ready", function () {
+			// Configure the project with Git.
+			shell.exec("git init --quiet");
+			shell.exec("git config --local user.name 'Testy Tester'");
+			shell.exec("git config --local user.email testy@testers.com");
+		})
+		.once("end", done);
 	});
 
 	it("creates a gulpfile", function (done) {
@@ -52,7 +63,25 @@ describe("node:app", function () {
 	});
 
 	it("creates a project manifest", function (done) {
-		assert.file("package.json");
+		var defaults = {
+			author      : "Testy Tester <testy@testers.com>",
+			description : undefined,
+			license     : "MIT",
+			name        : "tmp",
+			version     : "0.0.0"
+		};
+
+		var manifest;
+
+		assert.file(MANIFEST);
+		manifest = file.readJSON(MANIFEST);
+
+		_.each(defaults, function (value, name) {
+			expect(manifest[name], name).to.equal(value);
+		});
+		expect(manifest.scripts.test, "test").to.equal("gulp");
+		expect(manifest.scripts.coverage, "coverage").to.equal("gulp coverage");
+
 		done();
 	});
 
